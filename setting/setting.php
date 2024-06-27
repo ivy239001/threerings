fa0dae6c070c169647af07e2954723068cc97d77
LAPTOP-353KICB8\239005 <stoic1214@gmail.com>
2024-06-27 14:36:05 +0900

    ｔｍｔ

---
+++ setting/setting.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+if(isset($_SESSION['login'])==false){
+    print'ログインされていません。<br/>';
+    print'<a href="../login/login.php">ログイン画面へ</a>';
+    exit();
+}
+
+// エラーレポートをオンにする
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$servername = "172.16.3.130"; // データベースサーバーのIPアドレスまたはホスト名
+$username = "ivy_c239001"; // データベースユーザー名
+$password = ""; // データベースパスワード
+$dbname = "threerings"; // データベース名
+$port = 3306; // データベースのポート番号
+
+// データベース接続の作成
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// 接続チェック
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// SQLクエリの作成
+$sql = "SELECT name FROM login_02"
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="style.css"></script>
+    <title>Document</title>
+</head>
+<body>
+<ul>
+	<li><a class="active" href="#home">Home</a></li>
+	<li><a href="#news">News</a></li>
+	<li><a href="#contact">Contact</a></li>
+	<li><a href="#about">About</a></li>
+</ul>
+    
+    
+</body>
+</html>
