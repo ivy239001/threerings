19ebf36fc45c1299d56d8d4d829392a51e7e4ca0
IVY3135\239006 <planning207@gmail.com>
2024-06-28 14:47:08 +0900

    Error削除

--- login/register.php
+++ login/register.php
@@ -1,33 +1,36 @@
 <?php
-//フォームからの値をそれぞれ変数に代入
+// フォームからの値をそれぞれ変数に代入
 $name = $_POST['name'];
 $mail = $_POST['mail'];
-$pass = password_hash($_POST['pass'], PASSWORD_DEFAULT); // 変数名を $pass から $pw に変更
+$pass = password_hash($_POST['pass'], PASSWORD_DEFAULT); // パスワードをハッシュ化
 $dsn = "mysql:host=172.16.3.130; dbname=threerings; charset=utf8";
 $username = "ivy_c239001";
 $password = "";
+
 try {
     $dbh = new PDO($dsn, $username, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("接続エラー: " . $e->getMessage());
 }
 
-//フォームに入力されたmailがすでに登録されていないかチェック
+// フォームに入力されたメールがすでに登録されていないかチェック
 $sql = "SELECT * FROM login_02 WHERE mail = :mail";
 $stmt = $dbh->prepare($sql);
 $stmt->bindValue(':mail', $mail);
 $stmt->execute();
-$member = $stmt->fetch();
-if ($member['mail'] === $mail) {
+$member = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($member && $member['mail'] === $mail) {
     $msg = '同じメールアドレスが存在します。';
     $link = '<a href="signup.php" class="btn">戻る</a>';
 } else {
-    //登録されていなければinsert 
-    $sql = "INSERT INTO login_02(name, mail, pass) VALUES (:name, :mail, :pass)"; // :pass を :pw に変更
+    // 登録されていなければinsert
+    $sql = "INSERT INTO login_02(name, mail, pass) VALUES (:name, :mail, :pass)";
     $stmt = $dbh->prepare($sql);
     $stmt->bindValue(':name', $name);
     $stmt->bindValue(':mail', $mail);
-    $stmt->bindValue(':pass', $pass); // :pass を :pw に変更
+    $stmt->bindValue(':pass', $pass);
     $stmt->execute();
     $msg = '会員登録が完了しました';
     $link = '<a href="login_form.php" class="btn">ログインページ</a>';
