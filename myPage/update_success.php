d11dc521f6dc07fd6cf930eca65fcffc1d1ef2fe
LAPTOP-353KICB8\239005 <stoic1214@gmail.com>
2024-07-05 14:54:45 +0900

    パワポとマイページ更新完了画面

--- myPage/update_success.php
+++ myPage/update_success.php
@@ -5,6 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>更新完了</title>
 </head>
+<style>
+    body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+        flex-direction: column;
+        text-align: center;
+        background: linear-gradient(135deg, #ffffff, #000000);
+
+    }
+</style>
 <body>
     <h2>情報が更新されました</h2>
     <a href="../main/main.php">メイン画面へ戻る</a>
